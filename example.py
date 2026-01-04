3cd905624a0fee1872fbcdb201431f939c861835
xseeya <115592165+xseeya@users.noreply.github.com>
2026-01-04 08:40:15 -0800

    Update example.py

--- example.py
+++ example.py
@@ -120,7 +120,6 @@ twitch_miner.analytics(host="0.0.0.0", port=10000, refresh=5, days_ago=7)   # St
 
 twitch_miner.mine(
     [
-        "IKASPEROVSKAYA",
         "poisonika",
         "pattm0nster",
         "zeus",
